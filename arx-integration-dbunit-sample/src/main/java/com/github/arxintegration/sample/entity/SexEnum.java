79de9f522d23f045bf31f4076a3a767b17401986
Nenad Jevdjenic <nenad.jevdjenic@gmail.com>
2020-10-12 20:05:14 +0200

    add arx parts

---
+++ arx-integration-dbunit-sample/src/main/java/com/github/arxintegration/sample/entity/SexEnum.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2012, Trivadis AG, ASTRA. All rights reserved.
+ * 
+ * Project:   DM2010
+ * Product:   IVZ
+ * Module:    Data Services
+ */
+package com.github.arxintegration.sample.entity;
+
+/**
+ * Sex values for {@link Person}.
+ * 
+ * @author mht
+ */
+public enum SexEnum {
+    MALE, FEMALE;
+}
